c0e64deba2be0d377cd13214c27a9994c040a696
Eli Yale <elivyale@gmail.com>
2018-06-04 21:43:02 -0700

    added ncd computation in compute fitness function, should be working but need to run tests on compression

--- LZ77.py
+++ LZ77.py
@@ -1,166 +1,152 @@
-import math
-from bitarray import bitarray
+# coding=utf8
+#
+# The MIT License
+# 
+# Copyright (c) 2009 Olle Törnström studiomediatech.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# CREDIT: From an initial implementation in Python by Diogo Kollross, made 
+#         publicly available on http://www.geocities.com/diogok_br/lz77.
+#
+class Compressor:
+    
+    def __init__(self):
+        self.referencePrefix = "`"
+        self.referencePrefixCode = ord(self.referencePrefix)
+        self.referenceIntBase = 96
+        self.referenceIntFloorCode = ord(" ")
+        self.referenceIntCeilCode = self.referenceIntFloorCode + self.referenceIntBase - 1
+        self.maxStringDistance = self.referenceIntBase ** 2 - 1
+        self.minStringLength = 5
+        self.maxStringLength = self.referenceIntBase ** 1 - 1 + self.minStringLength
+        self.maxWindowLength = self.maxStringDistance + self.minStringLength;        
+        self.defaultWindowLength = 144
+        
+    def compress(self, data, windowLength = None):
+        """Compresses text data using the LZ77 algorithm."""
+        
+        if windowLength == None:
+            windowLength = self.defaultWindowLength
+    
+        compressed = ""
+        pos = 0
+        lastPos = len(data) - self.minStringLength
+        
+        while pos < lastPos:
+            
+            searchStart = max(pos - windowLength, 0);
+            matchLength = self.minStringLength
+            foundMatch = False
+            bestMatchDistance = self.maxStringDistance
+            bestMatchLength = 0
+            newCompressed = None
+            
+            while (searchStart + matchLength) < pos:
+                
+                m1 = data[searchStart : searchStart + matchLength]
+                m2 = data[pos : pos + matchLength]
+                isValidMatch = (m1 == m2 and matchLength < self.maxStringLength)
+                
+                if isValidMatch:
+                    matchLength += 1
+                    foundMatch = True
+                else:
+                    realMatchLength = matchLength - 1
+                    
+                    if foundMatch and realMatchLength > bestMatchLength:
+                        bestMatchDistance = pos - searchStart - realMatchLength
+                        bestMatchLength = realMatchLength
+                        
+                    matchLength = self.minStringLength
+                    searchStart += 1
+                    foundMatch = False
+                    
+            if bestMatchLength:
+                newCompressed = (self.referencePrefix + self.__encodeReferenceInt(bestMatchDistance, 2) + self.__encodeReferenceLength(bestMatchLength))
+                pos += bestMatchLength
+            else:
+                if data[pos] != self.referencePrefix:
+                    newCompressed = data[pos]
+                else:
+                    newCompressed = self.referencePrefix + self.referencePrefix
+                pos += 1    
+            
+            compressed += newCompressed
+        
+        return compressed + data[pos:].replace("`", "``")
+    
+    def decompress(self, data):
+        """Decompresses LZ77 compressed text data"""
+        
+        decompressed = ""
+        pos = 0
+        while pos < len(data):
+            currentChar = data[pos]
+            if currentChar != self.referencePrefix:
+                decompressed += currentChar
+                pos += 1
+            else:
+                nextChar = data[pos + 1]
+                if nextChar != self.referencePrefix:
+                    distance = self.__decodeReferenceInt(data[pos + 1 : pos + 3], 2)
+                    length = self.__decodeReferenceLength(data[pos + 3])
+                    start = len(decompressed) - distance - length
+                    end = start + length
+                    decompressed += decompressed[start : end]
+                    pos += self.minStringLength - 1
+                else:
+                    decompressed += self.referencePrefix
+                    pos += 2
+                    
+        return decompressed
 
-class LZ77Compressor:
-	"""
-	A simplified implementation of the LZ77 Compression Algorithm
-	"""
-	MAX_WINDOW_SIZE = 400
+    def __encodeReferenceInt(self, value, width):
+        if value >= 0 and value < (self.referenceIntBase ** width - 1):
+            encoded = ""
+            while value > 0:
+                encoded = chr((value % self.referenceIntBase) + self.referenceIntFloorCode) + encoded
+                value = int(value / self.referenceIntBase)
+            
+            missingLength = width - len(encoded)
+            for i in range(missingLength):
+                encoded = chr(self.referenceIntFloorCode) + encoded
+                
+            return encoded
 
-	def __init__(self, window_size=20):
-		self.window_size = min(window_size, self.MAX_WINDOW_SIZE) 
-		self.lookahead_buffer_size = 15 # length of match is at most 4 bits
-
-	def compress(self, data, output_file_path=None, verbose=False):
-		"""
-		Given the path of an input file, its content is compressed by applying a simple 
-		LZ77 compression algorithm. 
-
-		The compressed format is:
-		0 bit followed by 8 bits (1 byte character) when there are no previous matches
-			within window
-		1 bit followed by 12 bits pointer (distance to the start of the match from the 
-			current position) and 4 bits (length of the match)
-		
-		If a path to the output file is provided, the compressed data is written into 
-		a binary file. Otherwise, it is returned as a bitarray
-
-		if verbose is enabled, the compression description is printed to standard output
-		"""
-		i = 0
-		output_buffer = bitarray(endian='big')
-
-		while i < len(data):
-			#print i
-
-			match = self.findLongestMatch(data, i)
-
-			if match: 
-				# Add 1 bit flag, followed by 12 bit for distance, and 4 bit for the length
-				# of the match 
-				(bestMatchDistance, bestMatchLength) = match
-
-				output_buffer.append(True)
-				output_buffer.frombytes(chr(bestMatchDistance >> 4))
-				output_buffer.frombytes(chr(((bestMatchDistance & 0xf) << 4) | bestMatchLength))
-
-				if verbose:
-					print "<1, %i, %i>" % (bestMatchDistance, bestMatchLength),
-
-				i += bestMatchLength
-
-			else:
-				# No useful match was found. Add 0 bit flag, followed by 8 bit for the character
-				output_buffer.append(False)
-				output_buffer.frombytes(data[i])
-				
-				if verbose:
-					print "<0, %s>" % data[i],
-
-				i += 1
-
-		# fill the buffer with zeros if the number of bits is not a multiple of 8		
-		output_buffer.fill()
-
-		# write the compressed data into a binary file if a path is provided
-		if output_file_path:
-			try:
-				with open(output_file_path, 'wb') as output_file:
-					output_file.write(output_buffer.tobytes())
-					print "File was compressed successfully and saved to output path ..."
-					return None
-			except IOError:
-				print 'Could not write to output file path. Please check if the path is correct ...'
-				raise
-
-		# an output file path was not provided, return the compressed data
-		return output_buffer
-
-
-	def decompress(self, input_file_path, output_file_path=None):
-		"""
-		Given a string of the compressed file path, the data is decompressed back to its 
-		original form, and written into the output file path if provided. If no output 
-		file path is provided, the decompressed data is returned as a string
-		"""
-		data = bitarray(endian='big')
-		output_buffer = []
-
-		# read the input file
-		try:
-			with open(input_file_path, 'rb') as input_file:
-				data.fromfile(input_file)
-		except IOError:
-			print 'Could not open input file ...'
-			raise
-
-		while len(data) >= 9:
-
-			flag = data.pop(0)
-
-			if not flag:
-				byte = data[0:8].tobytes()
-
-				output_buffer.append(byte)
-				del data[0:8]
-			else:
-				byte1 = ord(data[0:8].tobytes())
-				byte2 = ord(data[8:16].tobytes())
-
-				del data[0:16]
-				distance = (byte1 << 4) | (byte2 >> 4)
-				length = (byte2 & 0xf)
-
-				for i in range(length):
-					output_buffer.append(output_buffer[-distance])
-		out_data =  ''.join(output_buffer)
-
-		if output_file_path:
-			try:
-				with open(output_file_path, 'wb') as output_file:
-					output_file.write(out_data)
-					print 'File was decompressed successfully and saved to output path ...'
-					return None 
-			except IOError:
-				print 'Could not write to output file path. Please check if the path is correct ...'
-				raise 
-		return out_data
-
-
-	def findLongestMatch(self, data, current_position):
-		""" 
-		Finds the longest match to a substring starting at the current_position 
-		in the lookahead buffer from the history window
-		"""
-		end_of_buffer = min(current_position + self.lookahead_buffer_size, len(data) + 1)
-
-		best_match_distance = -1
-		best_match_length = -1
-
-		# Optimization: Only consider substrings of length 2 and greater, and just 
-		# output any substring of length 1 (8 bits uncompressed is better than 13 bits
-		# for the flag, distance, and length)
-		for j in range(current_position + 2, end_of_buffer):
-
-			start_index = max(0, current_position - self.window_size)
-			substring = data[current_position:j]
-
-			for i in range(start_index, current_position):
-
-				repetitions = len(substring) / (current_position - i)
-
-				last = len(substring) % (current_position - i)
-
-				matched_string = data[i:current_position] * repetitions + data[i:i+last]
-
-				if matched_string == substring and len(substring) > best_match_length:
-					best_match_distance = current_position - i 
-					best_match_length = len(substring)
-
-		if best_match_distance > 0 and best_match_length > 0:
-			return (best_match_distance, best_match_length)
-		return None
-
-
-
-
+        else:
+            raise Exception("Reference value out of range: %d (width = %d)" % (value, width))
+        
+    def __encodeReferenceLength(self, length):
+        return self.__encodeReferenceInt(length - self.minStringLength, 1)
+    
+    def __decodeReferenceInt(self, data, width):
+        value = 0
+        for i in range(width):
+            value *= self.referenceIntBase
+            charCode = ord(data[i])
+            if charCode >= self.referenceIntFloorCode and charCode <= self.referenceIntCeilCode:
+                value += charCode - self.referenceIntFloorCode
+            else:
+                raise Exception("Invalid char code: %d" % charCode)
+            
+        return value
+    
+    def __decodeReferenceLength(self, data):
+        return self.__decodeReferenceInt(data, 1) + self.minStringLength
--- genetic.py
+++ genetic.py
@@ -1,6 +1,9 @@
 import random
 import argparse
-from LZ77 import LZ77Compressor
+from LZ77 import Compressor
+import bitarray
+from midi_strings import *
+import string #for getting list of ascii characters for alphabet
 
 #class to genetrate a string that resembles targetString using a genetic algorithm
 #with Nomralized Compression Distance as the Fitness function
@@ -8,117 +11,149 @@ from LZ77 import LZ77Compressor
 #individual is the NCD calcualted for the string as shown below
 #
 #initalization parameters:
-	#target_string = string that the algorithm will attempt to replicate
-	#gene_set = string comprised of all the possible sharacters in the alphabet for target_strings
-	#generation size = int for number of individuals in a generation
-	#n_reporduce = int for the number fo individuals in a generation that will reporduce, (determined by highest fitness)
-	#mutaion_rate = float for the rate at which individual mutations in the gene strings are introduced into the offspring
-	#corssover_rate = float for the percentage of parent1 chromosome that is taken in the child,
-	#	a value of 1, means all of parent 1 is taken, a value of 0.5 means the chromosome is crossed over halfway
-	#	!!!!might not implement this, sounds comlicated
+    #target_string = string that the algorithm will attempt to replicate
+    #gene_set = string comprised of all the possible sharacters in the alphabet for target_strings
+    #generation size = int for number of individuals in a generation
+    #n_reporduce = int for the number fo individuals in a generation that will reporduce, (determined by highest fitness)
+    #mutaion_rate = float for the rate at which individual mutations in the gene strings are introduced into the offspring
+    #corssover_rate = float for the percentage of parent1 chromosome that is taken in the child,
+    #    a value of 1, means all of parent 1 is taken, a value of 0.5 means the chromosome is crossed over halfway
+    #    !!!!might not implement this, sounds comlicated
 class Generator():
-	def __init__(self, target_string, gene_set, generation_size = 30, n_reproduce = 2, mutation_rate = 0.8, crossover_rate = 0.5, iterations = 30):
-		self.target_string = target_string
-		self.gene_set = gene_set
-		self.generation_size = generation_size
-		self.n_reproduce = n_reproduce
-		self.mutation_rate = mutation_rate
-		self.crossover_rate = crossover_rate
-		self.iterations = iterations
+    def __init__(self, target, gene_set, generation_size = 30, n_reproduce = 2, mutation_rate = 0.8, crossover_rate = 0.5, iterations = 30):
+        self.target = target
+        self.target_string = ''.join(target)
+        self.gene_set = gene_set
+        self.generation_size = generation_size
+        self.n_reproduce = n_reproduce
+        self.mutation_rate = mutation_rate
+        self.crossover_rate = crossover_rate
+        self.iterations = iterations
 
-		#the generation is initalized to an empty array
-		#self.generation = []
+        #the generation is initalized to an empty array
+        #self.generation = []
 
-	#begin genetic algorithm
-	def run(self):
+    #begin genetic algorithm
+    def run(self):
 
-		#first create generation_size random chromosomes and populate in the generation array
-		self.generation = [chromosome(genes = self._random_gene()) for x in range(self.generation_size)]
+        #first create generation_size random chromosomes and populate in the generation array
+        self.generation = [chromosome(genes = self._random_gene(), target=self.target) for x in range(self.generation_size)]
 
-		for i in range(self.iterations):
-			#sort this generation by individual fitness
-			self.generation.sort(key = lambda x: x.fitness, reverse=True)
+        for i in range(self.iterations):
+            #sort this generation by individual fitness
+            self.generation.sort(key = lambda x: x.fitness, reverse=True)
 
-			#restrict the generation to the n_reproduce strongest individuals as parents
-			parents = self.generation[:self.n_reproduce]
+            #restrict the generation to the n_reproduce strongest individuals as parents
+            parents = self.generation[:self.n_reproduce]
 
-			#crossover the parents to create children at the given crossover rate
-			children = [chromosome(genes = self._crossover(parents)) for x in range(self.generation_size - len(parents))]
+            #crossover the parents to create children at the given crossover rate
+            children = [chromosome(genes = self._crossover(parents), target=self.target) for x in range(self.generation_size - len(parents))]
 
-			#mutate the children according to the mutation rate
-			children = self._mutate(children)
+            #mutate the children according to the mutation rate
+            children = self._mutate(children)
 
-			self.generation = parents + children
+            self.generation = parents + children
 
-			verbose_print("Generation:", i)
-			for x in self.generation:
-				verbose_print("\tgenes:",x.genes, " fitness:", x.fitness)
+            verbose_print("Generation:", i)
+            for x in self.generation:
+                verbose_print("\tgenes:",x.genes, " fitness:", x.fitness)
 
 
 
-	def _random_gene(self):
-		return random.choices(self.gene_set, k = len(self.target_string))
+    def _random_gene(self):
+        return random.choices(self.gene_set, k = len(self.target))
 
-	#returns a new gene from random parents selected fron gene_pool
-	def _crossover(self, gene_pool):
-		parent1 = random.choice(gene_pool)
-		parent2 = random.choice(gene_pool)
+    #returns a new gene from random parents selected fron gene_pool
+    def _crossover(self, gene_pool):
+        parent1 = random.choice(gene_pool)
+        parent2 = random.choice(gene_pool)
 
-		crossover_point = int(round(self.crossover_rate * len(self.target_string)))
+        crossover_point = int(round(self.crossover_rate * len(self.target)))
 
-		#concatenate parent genes at crossover point
-		new_gene = parent1.genes[:crossover_point] + parent2.genes[crossover_point:]
+        #concatenate parent genes at crossover point
+        new_gene = parent1.genes[:crossover_point] + parent2.genes[crossover_point:]
 
-		return new_gene
+        return new_gene
 
-	def _mutate(self, gene_pool):
-		for child in gene_pool:
-			if random.randint(0,99) < (self.mutation_rate * 100):
-				child.genes[random.randint(0,len(child.genes)-1)] = random.choice(self.gene_set)
-				#if there is a mutation we need to recalculate fitness
-				child.set_fitness(child.compute_fitness())
+    def _mutate(self, gene_pool):
+        for child in gene_pool:
+            if random.randint(0,99) < (self.mutation_rate * 100):
+                child.genes[random.randint(0,len(child.genes)-1)] = random.choice(self.gene_set)
+                #if there is a mutation we need to recalculate fitness
+                child.set_fitness(child.compute_fitness())
 
-		return gene_pool
+        return gene_pool
 
 
 
 
 
 class chromosome():
-	def __init__(self, genes, fitness = 0):
-		self.genes = genes
-		self.fitness = self.compute_fitness()
+	#note chromosome needs to know the target, in order to calculate its fitness
+    def __init__(self, genes, target, fitness = 0,):
+        self.genes = genes
+        self.target = target
+        self.target_string = ''.join(target)
+        self.fitness = self.compute_fitness()
 
-	def compute_fitness(self):
-		#compute using NCD
+    def compute_fitness(self):
+        #compute using NCD
 
-		compressor = LZ77Compressor()
-		compressed_data = compressor.compress(self.genes)
+        #convert list of whatever to a bit array for passing to the compressor
+        gene_string = ''.join(self.genes)
+        print(gene_string:)
+        gene_bit_array = bitarray.bitarray()
+        gene_bit_array.frombytes(gene_string.encode('utf-8'))
+        
+        compressor = Compressor()
+        compressed_data = compressor.compress(data=gene_string)
+        
+        verbose_print("length of data:", len(gene_string), " length of compression: ", len(compressed_data))
 
-		verbose_print(len(compressed_data))
+        C_x_y = len(compressor.compress(gene_string+self.target_string))
+        C_x = len(compressor.compress(gene_string))
+        C_y_x = len(compressor.compress(self.target_string+gene_string))
+        C_y = len(compressor.compress(self.target_string))
+
+        ncd = max((C_x_y - C_x),(C_y_x - C_y))/max(C_x,C_y)
+
+        verbose_print("Nomralized Compression Distance:", ncd)
+
+        return ncd
 
 
-	def set_fitness(self, new_fitness):
-		self.fitness = new_fitness
+    def set_fitness(self, new_fitness):
+        self.fitness = new_fitness
 
 
 if __name__ == '__main__':
 
-	alphabet = 'abcd'
-	target = 'abcdabcdabcdabcd'
+    # with open("ex.txt", 'rb') as in_file:
+    # 	data = in_file.read()
+    # print(data)
+    # print(data[0])
+    # input()
 
-	parser = argparse.ArgumentParser(description='generate music with a genetic algorithm')
-	#parser.add_argument('dataset', metavar='dataset', nargs=1, type=str)
-	parser.add_argument('--verbose', dest='x', action='store_const', const=True, default=False)
+    target_string = mid64
 
-	args = parser.parse_args()
-	verbose = args.x
+    target = list(target_string)
+    alphabet = list(string.ascii_letters+string.digits)
 
-	global verbose_print
-	verbose_print = print if verbose else lambda *a, **k: None
+    # alphabet = ['a','b','c','d']
+    # target = ['a','b','c','d','a','b','c','d','a','b','c','d','a','b','c','d']
 
-	generator = Generator(target, alphabet)
-	generator.run()
+    parser = argparse.ArgumentParser(description='generate music with a genetic algorithm')
+    #parser.add_argument('dataset', metavar='dataset', nargs=1, type=str)
+    parser.add_argument('--verbose', dest='x', action='store_const', const=True, default=False)
+
+    args = parser.parse_args()
+    verbose = args.x
+
+    global verbose_print
+    verbose_print = print if verbose else lambda *a, **k: None
+
+    generator = Generator(target, alphabet)
+    generator.run()
 
 
 
